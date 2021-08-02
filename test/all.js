2faaf1b7b0ac7e68559f2caedba287cb9b196be1
simon-id <simon.id@protonmail.com>
2021-08-02 16:01:09 +0200

    add node core tests

---
+++ test/all.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const path = require('path');
+const child_process = require('child_process');
+
+// copied from node core
+const tests = [
+  'test-diagnostics-channel-has-subscribers.js',
+  // not including this one because it tests http server channels and AsyncLocalStorage
+  // test-diagnostics-channel-http-server-start.js
+  'test-diagnostics-channel-object-channel-pub-sub.js',
+  'test-diagnostics-channel-safe-subscriber-errors.js',
+  'test-diagnostics-channel-symbol-named.js',
+];
+
+for (const test of tests) {
+  const filePath = path.join(__dirname, 'core', test);
+
+  try {
+    child_process.execSync(`node ${filePath}`, { timeout: 1e3 });
+  } catch (err) {
+    process.exit(1);
+  }
+}
